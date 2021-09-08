14f2e3ca1d43f04c20cc304b8424fe007a32869e
Jaskeerat Singh <jaskeerat10000@gmail.com>
2021-09-08 15:48:17 +0530

    minor changes in the app

---
+++ JasPort3d/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import styleImport from 'vite-plugin-style-import'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/dist/',
+  build: {
+    chunkSizeWarningLimit:3000,
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+        }
+    }
+  }
+})
